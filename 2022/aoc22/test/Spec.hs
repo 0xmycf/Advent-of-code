702be6c41e83c10ec970df90d3f83b23c9797a1a
0xmycf <mycf.mycf.mycf@gmail.com>
2022-12-01 20:58:33 +0100

    :broom:

--- 2022/aoc22/test/Spec.hs
+++ 2022/aoc22/test/Spec.hs
@@ -1,43 +1,29 @@
-{-# LANGUAGE QuasiQuotes #-}
-import           Data.Text         (Text, unpack)
-import           Days.DayOne       (day1)
-import           NeatInterpolation (text)
-import           Solution          (Solution (..))
-import           Test.Hspec        (describe, hspec, it, shouldBe)
+import           Days.DayOne (day1)
+import           Finite      (unwrap)
+import           Solution    (Solution (..))
+import           Test.Hspec  (describe, hspec, it, shouldBe)
 
 main :: IO ()
 main = hspec $ do
   describe "Day One" $ do
-    let (pa, pb) = solveDay day1 testDayOne
-     in do
-        it "Part A -- 24000" $ do
-          pa `shouldBe` "24000"
-        it "Part B -- 45000" $ do
-          pb `shouldBe` "45000"
+    it "Part A -- 24000" $ do
+      (pa, _) <- solveDay day1
+      pa `shouldBe` "24000"
+    it "Part B -- 45000" $ do
+      (_, pb) <- solveDay day1
+      pb `shouldBe` "45000"
 
-testDayOne :: Text
-testDayOne =
-      [text|
-      1000
-      2000
-      3000
 
-      4000
-
-      5000
-      6000
-
-      7000
-      8000
-      9000
-
-      10000
-      |]
-
-solveDay :: Solution -> Text -> (String, String)
-solveDay Solution{..} file = (solvea, solveb)
+solveDay :: Solution -> IO (String, String)
+solveDay val@Solution{..} = do
+                             s1 <- solvea
+                             s2 <- solveb
+                             pure (s1,s2)
   where
-  parsed = common (unpack file)
-  solvea = partA  parsed
-  solveb = partB  parsed
+  file = readFile $ testPath ++ ( "day" ++ (show . (+1) . unwrap . Solution.day $ val) ++ ".txt")
+  parsed = common <$> file
+  solvea = partA  <$> parsed
+  solveb = partB  <$> parsed
 
+testPath :: FilePath
+testPath = "./input/test/"
