ee33e1c1df27c85ab979567036bad055df6b32d2
0xmycf <mycf.mycf.mycf@gmail.com>
2022-12-02 15:43:30 +0100

    cleanup

--- 2022/aoc22/src/Days/DayTwo.hs
+++ 2022/aoc22/src/Days/DayTwo.hs
@@ -22,7 +22,7 @@ import           Solution (Solution (..))
 -}
 
 day2 :: Solution
-day2 = Solution {day=finite 1, partA=flip partA2 score, partB=partB2, common=commonDayTwo.parseDayTwo}
+day2 = Solution {day=finite 1, partA=flip partA2 score, partB=partB2, common=parseDayTwo}
 
 -- | A for Rock, B for Paper, and C for Scissors
 -- X for Rock, Y for Paper, and Z for Scissors
@@ -38,14 +38,25 @@ parseDayTwo input = splitted
     read' _   = error "faulty string"
   toWho _          = error "faulty input"
 
--- | 1 for Rock, 2 for Paper, and 3 for Scissors
-scoreSymbol :: Symbol -> Int
-scoreSymbol Rock     = 1
-scoreSymbol Paper    = 2
-scoreSymbol Scissors = 3
+partA2 :: [Round Move] -> (Round Move -> (Int, Int)) -> Int
+partA2 rnds mapper = fst . foldr (\(a,b) (x,y) -> (a+x, b+y)) (0,0) $ fmap mapper rnds
 
-class Scorable a b | a -> b where
-  score :: a -> b
+partB2 :: [Round Move] -> Int
+partB2 = flip partA2 mapper
+  where
+  mapper :: Round Move -> (Int, Int)
+  mapper rnd = score $ fmap toP2 rnd
+    where
+    -- | X means you need to lose,
+    --   Y means you need to end the round in a draw, and
+    --   Z means you need to win
+    toP2 :: Move -> Symbol
+    toP2 m = let o = fromEnum $ _op rnd in
+             case m of
+              X -> toEnum $ (o + 2) `mod` 3
+              Y -> _op rnd
+              Z -> toEnum $ (o + 1) `mod` 3
+
 
 data Round a = Round
   { _you :: a
@@ -55,17 +66,20 @@ data Round a = Round
 data Move = X | Y | Z
   deriving (Show, Enum, Eq, Read)
 
+data Symbol = Rock | Paper | Scissors
+  deriving (Show, Enum, Eq)
+
 mToS :: Move -> Symbol
 mToS = toEnum . fromEnum
 
-data Symbol = Rock | Paper | Scissors
-  deriving (Show, Enum, Eq, Ord)
+class Scorable a b | a -> b where
+  score :: a -> b
 
 instance Scorable Symbol Int where
-  score = scoreSymbol
+    score = (+1) . fromEnum
 
 instance Scorable Move Int where
-  score = scoreSymbol . mToS
+  score = score . mToS
 
 instance (Enum b, Scorable b Int) => Scorable (Round b) (Int, Int) where
   score (Round you op) = (score you + a, score op + b)
@@ -76,27 +90,4 @@ instance (Enum b, Scorable b Int) => Scorable (Round b) (Int, Int) where
                    | (y + 1) `mod` 3 == o -> (0,6)
                    | (y + 2) `mod` 3 == o -> (6,0)
                    | otherwise            -> error "Not in bounds of Enum"
-
-
-commonDayTwo :: [Round Move] -> [Round Move]
-commonDayTwo = id
-
-partA2 :: [Round Move] -> (Round Move -> (Int, Int)) -> Int
-partA2 rnds mapper = fst . foldr (\(a,b) (x,y) -> (a+x, b+y)) (0,0) $ fmap mapper rnds
-
-partB2 :: [Round Move] -> Int
-partB2 = flip partA2 mapper
-  where
-  mapper :: Round Move -> (Int, Int)
-  mapper rnd = score $ fmap toP2 rnd
-    where
-    --  X means you need to lose,
-    --  Y means you need to end the round in a draw, and
-    --  Z means you need to win
-    toP2 :: Move -> Symbol
-    toP2 m = let o = fromEnum $ _op rnd in
-             case m of
-              X -> toEnum $ (o + 2) `mod` 3
-              Y -> _op rnd
-              Z -> toEnum $ (o + 1) `mod` 3
 
