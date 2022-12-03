a2fe430a3d20921c6bb3a7e795161a7f2e9a48d9
0xmycf <mycf.mycf.mycf@gmail.com>
2022-12-03 14:29:24 +0100

    cleanup day3

--- 2022/aoc22/src/Days/Day03.hs
+++ 2022/aoc22/src/Days/Day03.hs
@@ -1,12 +1,11 @@
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Days.Day03 (day3) where
-import           Data.Char (isLower, ord)
-import           Data.List (nub)
-import           Data.Set  (Set)
-import qualified Data.Set  as S
-import           Finite    (finite)
-import           Lib       (both, groupN)
-import           Solution  (Solution (..))
+import           Data.Char       (isLower, ord)
+import           Data.List.Split (chunksOf)
+import           Data.Set        (Set)
+import qualified Data.Set        as S
+import           Finite          (finite)
+import           Lib             (both, intersections)
+import           Solution        (Solution (..))
 
 day3 :: Solution
 day3 = Solution {day=finite 2, partA=partA1, partB=partB1, common=commonDayOne.parseDayOne}
@@ -17,22 +16,18 @@ parseDayOne  :: String -> [String]
 parseDayOne =  lines
 
 commonDayOne :: [String] -> [Rucksack]
-commonDayOne = map (both (map toNum) . \xs -> splitAt (length xs `div` 2) xs)
+commonDayOne = map (both (map toNum) . (splitAt =<< (flip div 2 . length))) -- \xs -> splitAt (length xs `div` 2) xs
   where
   toNum char
     | isLower char = ord char - 96
     | otherwise    = ord char - 38
 
 partA1 :: [Rucksack] -> Int
-partA1 = sum . map (head . S.toList . uncurry S.intersection . both S.fromList)
+partA1 = sum . map (S.findMin . uncurry S.intersection . both S.fromList)
 
 partB1 :: [Rucksack] -> Int
-partB1= sum  . map mapper . groupN 3 . map (nub . uncurry (++))
+partB1= sum  . map mapper . chunksOf 3 . map (S.fromList . uncurry (++))
   where
-  mapper :: [[Int]] -> Int
-  mapper (a:b:c:_) = head . S.toList $ sfl a \/ sfl b \/ sfl c
-  sfl = S.fromList
-
-(\/) :: Ord a => Set a -> Set a -> Set a
-(\/) = S.intersection
+  mapper :: [Set Int] -> Int
+  mapper  = S.findMin . intersections
 
