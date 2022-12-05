1f5dd5cb49d91a6454695d7c24869ae9a77719f9
0xmycf <mycf.mycf.mycf@gmail.com>
2022-12-05 21:03:56 +0100

    comments on each step

--- 2022/aoc22/src/Days/Day05.hs
+++ 2022/aoc22/src/Days/Day05.hs
@@ -42,35 +42,35 @@ type Input        = (Crates, Instructions)
 parseDay05  :: String -> (Crates, Instructions)
 parseDay05 = ((crates . head) &&& (instructions . last)) . T.splitOn "\n\n" . pack
   where
-  crates     = Map.fromList . zip [0..]
-                . fmap (T.intercalate "" . filter (not . T.null)
-                . init
-                . fmap (T.drop 1 . T.take 2 . T.strip))
-                . transpose
-                . fmap crate
-                . T.lines
-  crate  xs | T.null xs = []
-  crate  xs = let (f,s) = T.splitAt 4 xs in f : crate s
+  crates     = Map.fromList . zip [0..]                           -- make a map
+                . fmap (T.intercalate "" . filter (not . T.null)  -- filter all empty spots out, then combine Text chunks into one text
+                . init                                            -- remove the 1,2,3,4... at the bottom
+                . fmap (T.drop 1 . T.take 2 . T.strip))           -- remove whitespace then take the Char from the insde of a [A] chunk
+                . transpose                                       -- Transpose so that rows -> cols and we get the correct stacks
+                . fmap crate                                      -- transform each line into a crate
+                . T.lines                                         -- split the first chunk of the input into lines
+  crate  xs | T.null xs = []                                      -- if line is emtpy return the base case []
+  crate  xs = let (f,s) = T.splitAt 4 xs in f : crate s           -- if line is not empty then split after 4 chars and prepend to the recursive call of the rest string
   instructions  = fmap instruction . T.lines
-  instruction   = (\[_,c,f,t] -> Ins (fromInteger c) (finite (f - 1)) (finite (t - 1)))
-                . fmap ((read @Integer) . T.unpack) . T.splitOn ","
-                . T.replace " to " "," . T.replace " from " "," . T.replace "move " ","
+  instruction   = (\[_,c,f,t] -> Ins (fromInteger c) (finite (f - 1)) (finite (t - 1))) -- Create Instructon out of each line (each line also contains "" so we ignore it at the beginning)
+                . fmap ((read @Integer) . T.unpack) . T.splitOn ","                     -- split up the numbers, convert them into Integers
+                . T.replace " to " "," . T.replace " from " "," . T.replace "move " "," -- remove the words, and whitespace (dont judge)
 
 common05 :: Input -> (Text -> Text) -> Text
-common05 (c, []) _ = Map.foldr (\txt acc -> T.head txt `T.cons` acc) "" c
-common05 (c, instruction:xs) f = common05 (Map.union smallMap c, xs) f
+common05 (crates, []) _ = Map.foldr (\txt acc -> T.head txt `T.cons` acc) "" crates   -- if we dont have any instructions anymore we combine the map values into the final value
+common05 (crates, instruction:rest) f = common05 (Map.union newCrates crates, rest) f -- recursive call with the updated crates and the rest of the instructions
   where
-  keyto = fromInteger . unwrap $ _to instruction
-  keyfrom = fromInteger . unwrap $ _from instruction
-  gs f = c Map.! (fromInteger . unwrap $ f instruction)
-  fstack = gs _from
-  tbmoved = f $ T.take (_count instruction) fstack
-  oldStack = T.drop (_count instruction) fstack
-  updatedStack = tbmoved `T.append` gs _to
-  smallMap = Map.fromList [(keyto, updatedStack), (keyfrom, oldStack)]
+  keyto            = fromInteger . unwrap $ _to instruction                    -- The key of the stack we move to
+  keyfrom          = fromInteger . unwrap $ _from instruction                  -- The key of the stack we move from
+  getStack key     = crates Map.! key                                          -- helper function to get the stack for a given key
+  fromStack        = getStack keyfrom                                          -- the stack from which we move
+  toBeMoved        = f $ T.take (_count instruction) fromStack                 -- The stack that gets moved, f apllied to it
+  droppedFromStack = T.drop (_count instruction) fromStack                     -- the new fromStack (dropped the creates that get moved)
+  updatedStack     = toBeMoved `T.append` getStack keyto                       -- the updated Stack which we move to
+  newCrates        = Map.fromList [(keyto, updatedStack), (keyfrom, droppedFromStack)] -- helper map to unify with old creates map
 
 partA1 :: Input -> Text
-partA1 input = common05 input T.reverse
+partA1 input = common05 input T.reverse -- in part a we need to reverse the stack
 
 partB1 :: Input -> Text
-partB1 input = common05 input id
+partB1 input = common05 input id -- in part b we leave the stacks as they were
