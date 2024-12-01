18d30fffaaf6db14f5562aade9ce7f2c809d3477
0xmycf <mycf.mycf.mycf@gmail.com>
2024-12-01 14:24:22 +0100

    2024 day1a bash

---
+++ 2024/day01/day01.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+INPUT="../input/day1.txt"
+# INPUT="./test.txt"
+
+one=$(cut -d " " -f 1 $INPUT | sort)
+two=$(cut -d " " -f 4 $INPUT | sort)
+
+read -r -a array1 <<< "$(echo "$one" | tr '\n' " ")"
+read -r -a array2 <<< "$(echo "$two" | tr '\n' " ")"
+
+echo "doing part a"
+
+total=0
+for idx in "${!array1[@]}"; do
+    elem1=${array1[$idx]}
+    elem2=${array2[$idx]}
+    diff=$((elem1 - elem2))
+    if [ "$diff" -le "0" ]; then
+        diff=$((-1 * diff)) 
+    fi 
+    total=$((total + diff))
+done
+
+echo $total
+
+echo "doing part b"
