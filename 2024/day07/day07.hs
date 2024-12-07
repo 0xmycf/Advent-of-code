73f7316c12b0c16a6a5cad2a29aa157a38d19ad8
0xmycf <mycf.mycf.mycf@gmail.com>
2024-12-07 19:15:08 +0100

    2024 day 7b haskell (not c sorry)

--- 2024/day07/day07.hs
+++ 2024/day07/day07.hs
@@ -5,8 +5,8 @@ module Main where
 import           Control.Monad   (guard)
 import           Data.List       (foldl', nub)
 import           Data.List.Split (splitOn)
-import           Data.Map        (Map)
-import qualified Data.Map        as Map
+import           Data.Map.Lazy   (Map)
+import qualified Data.Map.Lazy   as Map
 
 input :: IO [String]
 input = lines <$> readFile "../input/day07.txt"
@@ -14,17 +14,24 @@ input = lines <$> readFile "../input/day07.txt"
 type Input = [(Int, [Int])]
 
 parse :: [String] -> [(Int, [Int])]
-parse lines = [ (first, rest) | line <- lines 
+parse lines = [ (first, rest) | line <- lines
               , let [read @Int->first, parseRest->rest] = splitOn ":" line ]
   where parseRest xs = let s = filter (not . null) $ splitOn " " xs in fmap (read @Int) s
 
-ops :: Num a => [a -> a -> a]
-ops = [(+), (*)]
+-- use these instead to do part 1
+opsA :: Num a => [a -> a -> a]
+opsA = [(+), (*)]
+
+-- use these for part 2
+ops :: [Int -> Int -> Int]
+ops = [(+), (*), (|||)]
+  where
+    (show->a) ||| (show->b) = read (a <> b)
 
 solve :: Input -> Int
 solve lines = sum  do
   (target, x:xs) <- lines
-  let foo = foldl' (\acc val -> fmap ($ val) ops <*> acc) [x] xs
+  let foo = foldl' (\acc val -> fmap (`flip` val) ops <*> acc) [x] xs
   guard (target `elem` foo)
   pure target
 
