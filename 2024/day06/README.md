5eab87898e52805e9c0f418e15125b709fd11f89
0xmycf <mycf.mycf.mycf@gmail.com>
2024-12-08 00:50:37 +0100

    2024 day 6b plan solution and plan rendering

--- 2024/day06/README.md
+++ 2024/day06/README.md
@@ -1,3 +1,76 @@
 # Day06
 
 First thing I learned is that `dart create` creates a ton of files I probably wont need. 
+
+## Part 2 (Notes for later)
+
+Part b is not that hard, we just need to find certain intersections:
+
+```txt
+....#.....
+....+->-+#
+....|...|.
+..#.^...|.
+....|..#v.
+....|...|.
+.#-X--<-+.
+........#.
+#.........
+......#...
+```
+
+Here `X` is an intersection of a row that walks left "^-<-+" and a line that 
+walks up. Left -> Up is fine, we can put a box there.
+
+One just needs to be sure that the rest of the line is a closed circle and does not
+go into the abyss.
+
+```txt
+....#.....
+....+->-+#
+....|...|.
+..#.^...|.
+..|-+>-#v. <-- here
+..^.|.|.|.
+.#---<+-+.
+.^->--+-#.
+#---<-vv..
+......#|..
+```
+
+If we come from the left (walk towards right), then
+we would end up going just straight down, which would not circle back.
+
+If we come from down (walk towards up), then
+we would shortcut the normal root.
+
+---
+
+We can translate this into a graph problem:
+
+1. Instead of walking step by step, we walk segment by segment[^1] (these are our **Edges**)
+   We should also label them with a unique label and follow some ordering 
+   (e.g. if we walk up *then* right, *up* should be labeled $k$ and *right* should be
+   labeled $m$, with $k < m$) 
+2. The boxes are now the **Vertrices**
+
+We now need find new Vertricies that yield from our Edge (with id $k$) into a 
+    different Edge $m$, where $k < m$.
+  If we have $k < m$ then we have a loop.
+
+To find these new Edges we only need to check transitions that follow 
+    the behaviour of the guard.
+  If the current Edge (Segement) is oriented towards *going up*,
+    then we must only look at Edges (Segments) wich are *going right*, 
+    as we cannot turn left or 90° back.
+
+We can narrow the possible Edges (Segments) by "scanning" for 
+    obstacles along the way.
+  If we have one Segement from $(4,2)$ to $(4,6)$ (*up*),
+    then any *left*-facing Segment above $y$-level $6$ can be ignored.
+  There is no way to reach them once we hit $y=6$,
+    as we have to turn *right* (walk towards *left*) then.
+
+---
+
+[^1]: This should also speed up part 1
--- 2024/day06/day06.dart
+++ 2024/day06/day06.dart
@@ -5,16 +5,126 @@
 import 'dart:io';
 
 Future<void> main(List<String> arguments) async {
-  var map = File("../input/day06.txt");
-  // var map = File("./test.txt");
+  // var map = File("../input/day06.txt");
+  var map = File("./test.txt");
   var lines = await map.readAsLines();
-  var board = Board.fromString(lines);
+  var board = Board.fromString(lines, LabelOverseer.get());
+  board.partOne();
 
-  print(board.withOtherBuffer(() => board.partOne(true)));
-  // print(board.withOtherBuffer(() => board.partOne()));
+  // Renderable.withOtherBuffer(() {
+  //   // board.render();
+  //   var seg = Segement(from: Pos(1, 1), to: Pos(1, 11), direction: Dir.north);
+  //   var seg2 = Segement(from: Pos(1, 1), to: Pos(11, 1), direction: Dir.east);
+  //   var seg3 =
+  //       Segement(from: Pos(11, 11), to: Pos(11, 1), direction: Dir.south);
+  //   var seg4 = Segement(from: Pos(11, 11), to: Pos(1, 11), direction: Dir.west);
+  //   for (var s in [seg, seg2, seg3, seg4]) {
+  //     s.render();
+  //   }
+  //   Pos(11, 11).render();
+  //   Pos(1, 1).render();
+  //   Pos(1, 11).render();
+  //   Pos(11, 1).render();
+  // });
 }
 
-class Pos {
+class LabelOverseer {
+  Label _last = Label(0);
+  // ignore: non_constant_identifier_names
+  static final LabelOverseer INSTANCE = LabelOverseer();
+  Label next() {
+    var label = Label(_last.id);
+    _last = label;
+    return label;
+  }
+
+  static LabelOverseer get() {
+    return INSTANCE;
+  }
+}
+
+class Label {
+  final int id;
+  Label(this.id);
+}
+
+void moveCursor(int line, int column) {
+  // idk why line must be modified
+  // stdout.write("\x1b[${line + 1};${column}H");
+  stdout.write("\x1b[$line;${column}H");
+}
+
+abstract interface class Renderable {
+  void render();
+  static final String RED_X = "\x1b[31mX\x1b[0m";
+
+  static T withOtherBuffer<T>(T Function() fn) {
+    // enable alterantive buffer
+    stdout.write("\x1b 7"); // save cursor
+    stdout.write("\x1b[?25l"); // makes cursor invisible
+    stdout.write("\x1b[?1049h");
+    stdout.write("\x1b[H"); // move cursor to home position
+    var ret = fn();
+    stdout.write("\x1b[?1049l");
+    stdout.write("\x1b[?25h"); // makes cursor visible again
+    stdout.write("\x1b 8"); // restore cursor
+    stdout.write("\x1b[J");
+    return ret;
+  }
+}
+
+class Segement implements Renderable {
+  final Pos from;
+  final Pos to;
+  final Dir direction;
+
+  Segement({required this.from, required this.to, required this.direction})
+      : assert(switch (direction) {
+          Dir.north => from.x == to.x && from.y <= to.y,
+          Dir.south => from.x == to.x && from.y >= to.y,
+          Dir.west => from.y == to.y && from.x >= to.x,
+          Dir.east => from.y == to.y && from.x <= to.x,
+        });
+
+  (int min, int max) bounds() {
+    return switch (direction) {
+      Dir.north || Dir.south => (from.y, to.y),
+      Dir.west || Dir.east => (from.x, to.x),
+    };
+  }
+
+  int length() {
+    return switch (direction) {
+      Dir.north || Dir.south => (to.y - from.y),
+      Dir.east || Dir.west => (to.x - from.x),
+    }
+        .abs();
+  }
+
+  @override
+  String toString() {
+    return "Segement(from: $from, to: $to, dir: $direction, length: ${length()})";
+  }
+
+  @override
+  void render() {
+    var (min, max) = bounds();
+    if (direction case Dir.south || Dir.west) {
+      (min, max) = (max, min);
+    }
+    for (var i = min; i <= max; i++) {
+      switch (direction) {
+        case Dir.north || Dir.south:
+          moveCursor(i, from.x);
+        case Dir.east || Dir.west:
+          moveCursor(from.y, i);
+      }
+      stdout.write(direction.toString());
+    }
+  }
+}
+
+class Pos implements Renderable {
   final int x;
   final int y;
 
@@ -32,24 +142,39 @@ class Pos {
   int get hashCode => Object.hash(x, y);
 
   Pos move(Dir dir) {
-    switch (dir) {
-      case Dir.north:
-        return Pos(x - 1, y); // uh we are in Austrailia ig
-      case Dir.south:
-        return Pos(x + 1, y);
-      case Dir.east:
-        return Pos(x, y + 1);
-      case Dir.west:
-        return Pos(x, y - 1);
-    }
+    return switch (dir) {
+      Dir.north => Pos(x - 1, y), // uh we are in Austrailia ig
+      Dir.south => Pos(x + 1, y),
+      Dir.east => Pos(x, y + 1),
+      Dir.west => Pos(x, y - 1),
+    };
   }
 
   isOn(Board board) {
     return x >= 1 && y >= 1 && x <= board.height && y <= board.width;
   }
+
+  @override
+  void render() {
+    moveCursor(y, x); // TODO does this work?
+    stdout.write(Renderable.RED_X);
+  }
 }
 
-enum Dir { north, south, west, east }
+enum Dir {
+  north,
+  south,
+  west,
+  east;
+
+  @override
+  String toString() {
+    return switch (this) {
+      Dir.north || Dir.south => "|",
+      Dir.east || Dir.west => "-",
+    };
+  }
+}
 
 class Guard {
   Pos pos;
@@ -60,22 +185,12 @@ class Guard {
   Guard.fromInts(int x, int y) : this(Pos(x, y), Dir.north, {Pos(x, y)});
   @override
   String toString() {
-    String ret = "";
-    switch (dir) {
-      case Dir.north:
-        ret = '^';
-        break;
-      case Dir.south:
-        ret = 'v';
-        break;
-      case Dir.east:
-        ret = '>';
-        break;
-      case Dir.west:
-        ret = '<';
-        break;
-    }
-    return ret;
+    return switch (dir) {
+      Dir.north => '^',
+      Dir.south => 'v',
+      Dir.east => '>',
+      Dir.west => '<',
+    };
   }
 
   void move([Dir? argdir]) {
@@ -92,16 +207,12 @@ class Guard {
     switch (dir) {
       case Dir.north:
         dir = Dir.east;
-        break;
       case Dir.east:
         dir = Dir.south;
-        break;
       case Dir.south:
         dir = Dir.west;
-        break;
       case Dir.west:
         dir = Dir.north;
-        break;
     }
   }
 
@@ -114,22 +225,22 @@ class Guard {
   }
 }
 
-class Board {
+class Board implements Renderable {
   int width;
   int height;
   Guard guard;
-  List<Pos> obstacles;
   Map<Pos, bool> obstaclesMap;
+  LabelOverseer overseer;
 
-  factory Board._internal(
-      int width, int height, List<Pos> positions, Guard guard) {
-    return Board(width, height, positions,
-        {for (var elem in positions) elem: true}, guard);
+  factory Board._internal(int width, int height, List<Pos> positions,
+      Guard guard, LabelOverseer overseer) {
+    return Board(width, height, {for (var elem in positions) elem: true}, guard,
+        overseer);
   }
 
-  Board(this.width, this.height, this.obstacles, this.obstaclesMap, this.guard);
+  Board(this.width, this.height, this.obstaclesMap, this.guard, this.overseer);
 
-  factory Board.fromString(List<String> lines) {
+  factory Board.fromString(List<String> lines, LabelOverseer overseer) {
     List<Pos> positions = [];
     int width = 0;
     int height = 0;
@@ -146,38 +257,27 @@ class Board {
         }
       }
     }
-    return Board._internal(width, height, positions, guard);
-  }
-
-  T withOtherBuffer<T>(T Function() fn) {
-    // enable alterantive buffer
-    stdout.write("\x1b 7"); // save cursor
-    stdout.write("\x1b[?25l"); // makes cursor invisible
-    stdout.write("\x1b[?1049h");
-    stdout.write("\x1b[H"); // move cursor to home position
-    var ret = fn();
-    stdout.write("\x1b[?1049l");
-    stdout.write("\x1b[?25h"); // makes cursor visible again
-    stdout.write("\x1b 8"); // restore cursor
-    stdout.write("\x1b[J");
-    return ret;
+    return Board._internal(width, height, positions, guard, overseer);
   }
 
   /// renders the map on the console
+  @override
   void render() {
     visit((pos) {
-      if (obstaclesMap.containsKey(pos)) {
-        stdout.write('#');
-      } else if (guard.pos == pos) {
-        stdout.write(guard.toString());
-      } else {
-        stdout.write(".");
-      }
+      stdout.write(getField(pos));
     }, (x) {
       if (x != height) {
         stdout.write("\n");
       }
     });
+  }
+
+  void reRender(Pos prev, Pos curr) {
+    for (var pos in [prev, curr]) {
+      moveCursor(pos.x, pos.y); // this works?
+      stdout.write(getField(pos));
+    }
+    sleep(Duration(milliseconds: 80));
   }
 
   String getField(Pos pos) {
@@ -186,21 +286,10 @@ class Board {
     } else if (guard.pos == pos) {
       return guard.toString();
     } else if (guard.visited.contains(pos)) {
-      return "\x1b[31mX\x1b[0m";
+      return Renderable.RED_X;
     } else {
       return ".";
     }
-  }
-
-  void reRender(Pos prev, Pos curr) {
-    moveCursor(line, column) => stdout.write("\x1b[${line + 1};${column}H");
-
-    for (var pos in [prev, curr]) {
-      moveCursor(pos.x, pos.y);
-      stdout.write(getField(pos));
-    }
-    // sleep(Duration(milliseconds: 150));
-    sleep(Duration(milliseconds: 80));
   }
 
   int partOne([bool withGraphics = false]) {
@@ -237,16 +326,7 @@ class Board {
     final buf = StringBuffer();
     for (var x = 1; x <= height; x++) {
       for (var y = 1; y <= width; y++) {
-        var curr = Pos(x, y);
-        if (guard.pos == curr) {
-          buf.write(guard.toString());
-          continue;
-        }
-        if (obstaclesMap.containsKey(curr)) {
-          buf.write("#");
-        } else {
-          buf.write(".");
-        }
+        buf.write(getField(Pos(x, y)));
       }
       if (x != height) {
         buf.writeln();
