370a31b296203e985740884887179d45ffbbd4a3
0xmycf <mycf.mycf.mycf@gmail.com>
2024-12-08 01:48:11 +0100

    2024 day 6b fix coords

--- 2024/day06/day06.dart
+++ 2024/day06/day06.dart
@@ -9,22 +9,37 @@ Future<void> main(List<String> arguments) async {
   var map = File("./test.txt");
   var lines = await map.readAsLines();
   var board = Board.fromString(lines, LabelOverseer.get());
-  board.partOne();
+
+  Renderable.withOtherBuffer(() {
+    var res = board.partOne(withGraphics: true);
+    moveCursor(12, 1);
+    stdout.write(res);
+    int line = 13;
+    for (final s in board.segments) {
+      s.render();
+      moveCursor(line++, 1);
+      stdout.write(s);
+    }
+    sleep(Duration(minutes: 3));
+  });
 
   // Renderable.withOtherBuffer(() {
   //   // board.render();
-  //   var seg = Segement(from: Pos(1, 1), to: Pos(1, 11), direction: Dir.north);
-  //   var seg2 = Segement(from: Pos(1, 1), to: Pos(11, 1), direction: Dir.east);
-  //   var seg3 =
-  //       Segement(from: Pos(11, 11), to: Pos(11, 1), direction: Dir.south);
-  //   var seg4 = Segement(from: Pos(11, 11), to: Pos(1, 11), direction: Dir.west);
-  //   for (var s in [seg, seg2, seg3, seg4]) {
-  //     s.render();
-  //   }
+  //   var seg = Segment(from: Pos(6, 8), to: Pos(6, 3), direction: Dir.north);
+  //   // var seg2 = Segment(from: Pos(1, 1), to: Pos(11, 1), direction: Dir.west);
+  //   // var seg3 = Segment(from: Pos(11, 11), to: Pos(11, 1), direction: Dir.north);
+  //   // var seg4 = Segment(from: Pos(11, 11), to: Pos(1, 11), direction: Dir.east);
+  //   // for (var s in [seg, seg2, seg3, seg4]) {
+  //   //   s.render();
+  //   // }
+  //   seg.render();
   //   Pos(11, 11).render();
   //   Pos(1, 1).render();
   //   Pos(1, 11).render();
   //   Pos(11, 1).render();
+  //   moveCursor(20, 1);
+  //   print(seg);
+  //   sleep(Duration(minutes: 10));
   // });
 }
 
@@ -56,6 +71,7 @@ void moveCursor(int line, int column) {
 
 abstract interface class Renderable {
   void render();
+  // ignore: non_constant_identifier_names
   static final String RED_X = "\x1b[31mX\x1b[0m";
 
   static T withOtherBuffer<T>(T Function() fn) {
@@ -73,17 +89,50 @@ abstract interface class Renderable {
   }
 }
 
-class Segement implements Renderable {
+class SegmentBuilder {
+  Pos? from;
+  Pos? to;
+  Dir direction;
+
+  SegmentBuilder(this.direction);
+
+  SegmentBuilder from_(Pos from) {
+    this.from = from;
+    return this;
+  }
+
+  SegmentBuilder to_(Pos to) {
+    this.to = to;
+    return this;
+  }
+
+  Segment build([Segment? next]) {
+    if (from != null && to != null) {
+      var seg = Segment(from: from as Pos, to: to as Pos, direction: direction);
+      seg.next = next;
+      return seg;
+    }
+    throw "from $from or to $to is null";
+  }
+
+  @override
+  String toString() {
+    return "SegmentBuilder(from: $from, to: $to, dir: $direction)";
+  }
+}
+
+class Segment implements Renderable {
   final Pos from;
   final Pos to;
   final Dir direction;
+  Segment? next;
 
-  Segement({required this.from, required this.to, required this.direction})
+  Segment({required this.from, required this.to, required this.direction})
       : assert(switch (direction) {
-          Dir.north => from.x == to.x && from.y <= to.y,
-          Dir.south => from.x == to.x && from.y >= to.y,
-          Dir.west => from.y == to.y && from.x >= to.x,
-          Dir.east => from.y == to.y && from.x <= to.x,
+          Dir.north => from.x == to.x && from.y >= to.y,
+          Dir.south => from.x == to.x && from.y <= to.y,
+          Dir.west => from.y == to.y && from.x <= to.x,
+          Dir.east => from.y == to.y && from.x >= to.x,
         });
 
   (int min, int max) bounds() {
@@ -101,6 +150,15 @@ class Segement implements Renderable {
         .abs();
   }
 
+  static Segment link(List<Segment> segments) {
+    var curr = segments[0];
+    for (var i = 1; i < segments.length; i++) {
+      curr.next = segments[i];
+      curr = segments[i];
+    }
+    return segments[0];
+  }
+
   @override
   String toString() {
     return "Segement(from: $from, to: $to, dir: $direction, length: ${length()})";
@@ -109,7 +167,7 @@ class Segement implements Renderable {
   @override
   void render() {
     var (min, max) = bounds();
-    if (direction case Dir.south || Dir.west) {
+    if (direction case Dir.north || Dir.east) {
       (min, max) = (max, min);
     }
     for (var i = min; i <= max; i++) {
@@ -143,10 +201,10 @@ class Pos implements Renderable {
 
   Pos move(Dir dir) {
     return switch (dir) {
-      Dir.north => Pos(x - 1, y), // uh we are in Austrailia ig
-      Dir.south => Pos(x + 1, y),
-      Dir.east => Pos(x, y + 1),
-      Dir.west => Pos(x, y - 1),
+      Dir.north => Pos(x, y - 1), // console coords are flipped on the x axis
+      Dir.south => Pos(x, y + 1),
+      Dir.east => Pos(x + 1, y),
+      Dir.west => Pos(x - 1, y),
     };
   }
 
@@ -156,8 +214,13 @@ class Pos implements Renderable {
 
   @override
   void render() {
-    moveCursor(y, x); // TODO does this work?
+    moveCursor(y, x); // line and col
     stdout.write(Renderable.RED_X);
+  }
+
+  @override
+  String toString() {
+    return "Pos($x, $y)";
   }
 }
 
@@ -231,6 +294,8 @@ class Board implements Renderable {
   Guard guard;
   Map<Pos, bool> obstaclesMap;
   LabelOverseer overseer;
+  List<Segment> segments = [];
+  Segment? pathRoot;
 
   factory Board._internal(int width, int height, List<Pos> positions,
       Guard guard, LabelOverseer overseer) {
@@ -249,12 +314,12 @@ class Board implements Renderable {
       height++;
       width = 0;
       for (var c in line.split('')) {
-        width++;
         if (c == '#') {
-          positions.add(Pos(height, width));
+          positions.add(Pos(width, height));
         } else if (c == '^') {
-          guard = Guard.fromInts(height, width);
+          guard = Guard.fromInts(width, height);
         }
+        width++;
       }
     }
     return Board._internal(width, height, positions, guard, overseer);
@@ -265,8 +330,8 @@ class Board implements Renderable {
   void render() {
     visit((pos) {
       stdout.write(getField(pos));
-    }, (x) {
-      if (x != height) {
+    }, (y) {
+      if (y != height) {
         stdout.write("\n");
       }
     });
@@ -274,7 +339,7 @@ class Board implements Renderable {
 
   void reRender(Pos prev, Pos curr) {
     for (var pos in [prev, curr]) {
-      moveCursor(pos.x, pos.y); // this works?
+      moveCursor(pos.y, pos.x);
       stdout.write(getField(pos));
     }
     sleep(Duration(milliseconds: 80));
@@ -292,43 +357,55 @@ class Board implements Renderable {
     }
   }
 
-  int partOne([bool withGraphics = false]) {
+  int partOne({bool withGraphics = false}) {
     if (withGraphics) {
       render();
     }
     Pos prev = guard.pos;
+    SegmentBuilder sb = SegmentBuilder(Dir.north);
+    sb.from_(guard.pos);
     while (guard.isOn(this)) {
       prev = guard.pos;
       Pos next = guard.peek();
       if (obstaclesMap.containsKey(next)) {
         guard.rotateRight();
+        sb.to_(guard.pos);
+        segments.add(sb.build());
+        sb = SegmentBuilder(guard.dir);
+        sb.from_(guard.pos);
       }
       guard.move();
       if (withGraphics) {
         reRender(prev, guard.pos);
       }
+      // print(segments);
     }
+    pathRoot = Segment.link(segments);
     return guard.visited.where((pos) => pos.isOn(this)).length;
+  }
+
+  int partTwo({bool withGraphics = false}) {
+    return 0;
   }
 
   /// visit each node and apply fn on the current position
   void visit(void Function(Pos) fn, void Function(int) outerLoop) {
-    for (var x = 1; x <= height; x++) {
-      for (var y = 1; y <= width; y++) {
+    for (var y = 1; y <= height; y++) {
+      for (var x = 1; x <= width; x++) {
         fn(Pos(x, y));
       }
-      outerLoop(x);
+      outerLoop(y);
     }
   }
 
   @override
   String toString() {
     final buf = StringBuffer();
-    for (var x = 1; x <= height; x++) {
-      for (var y = 1; y <= width; y++) {
+    for (var y = 1; y <= height; y++) {
+      for (var x = 1; x <= width; x++) {
         buf.write(getField(Pos(x, y)));
       }
-      if (x != height) {
+      if (y != height) {
         buf.writeln();
       }
     }
