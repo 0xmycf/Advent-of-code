7bd4f793cc5d0f27a341dc8ccf6bdba243c69adf
0xmycf <mycf.mycf.mycf@gmail.com>
2024-12-20 23:37:31 +0100

    2024 day 19 b lua

--- 2024/day19/day19_v2.lua
+++ 2024/day19/day19_v2.lua
@@ -5,23 +5,15 @@ end
 -- https://stackoverflow.com/questions/9168058/how-to-dump-a-table-to-console
 local function dump(o)
   if type(o) == 'table' then
-    local s = '{ '
+    local s = '{\n'
     for k, v in pairs(o) do
       if type(k) ~= 'number' then k = '"' .. k .. '"' end
-      s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
+      s = s .. '  [' .. k .. '] = ' .. dump(v) .. ',\n'
     end
     return s .. '} '
   else
     return tostring(o)
   end
-end
-
-local function len(t)
-  local sum = 0
-  for _, _ in pairs(t) do
-    sum = sum + 1
-  end
-  return sum
 end
 
 local function read_file(path)
@@ -59,9 +51,9 @@ local sum = 0
 
 -- lets write a helper file for proper regex
 
-local pyfile = io.open("helper.py", "w+")
+--[[ local pyfile = io.open("helper.py", "w+")
 
-local content = "import re, sys\n\ns = sys.argv[1]\npattern = \"("
+local content = "import re, sys\n\ns = sys.argv[1]\npattern = r\"("
 for i, value in pairs(parsed) do
   if i == 1 then
     content = content .. value
@@ -85,10 +77,50 @@ for i, line in pairs(foo) do
   local status = os.execute("python helper.py " .. line)
   -- print("status is: " .. status)
   if status == 0 then
-    sum = sum +1
+    sum = sum + 1
   end
 
   ::continue::
 end
 
 print(sum)
+
+sum = 0 ]]
+
+-- part b
+
+local cache = {}
+
+local function count_matches(s)
+  if cache[s] then
+    return cache[s]
+  end
+
+  if s == "" then
+    return 1
+  end
+
+  local total = 0
+  for _, prefix in pairs(parsed) do
+    if starts(s, prefix) then
+      total = total + count_matches(string.sub(s, string.len(prefix) + 1, string.len(s)))
+    end
+  end
+  cache[s] = total
+  return total
+end
+
+for i, value in pairs(foo) do
+  if i == 1 or value == "" then
+    goto continue
+  end
+
+  -- print(value)
+  sum = sum + count_matches(value)
+
+  ::continue::
+end
+
+--print(dump(cache))
+
+print(string.format("%18.0f",sum))
